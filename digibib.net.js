a113cd8b1f86cea2d8eadcca6207b0a2a368c27b
Heiko Jansen <jansen@hbz-nrw.de>
2012-03-13 10:01:51 +0100

    Committed the version of the translator as was added to the zotero repository

--- digibib.net.js
+++ digibib.net.js
@@ -1,14 +1,15 @@
 {
-	"translatorID":"e99bd723-39e6-418c-9524-390dbc583e08",
-	"translatorType":4,
-	"label":"digibib.net",
-	"creator":"Heiko Jansen (hbz), Ingolf Kuss (hbz), Bernhard Assmann (hbz)",
-	"target":"https?://.*\\.digibib\\.net/(Digibib|jumpto|opensearch)",
-	"minVersion":"1.0",
-	"maxVersion":"",
-	"priority":100,
-	"inRepository":false,
-	"lastUpdated":"2012-03-09 10:15:00"
+	"translatorID": "e99bd723-39e6-418c-9524-390dbc583e08",
+	"translatorType": 4,
+	"label": "digibib.net",
+	"creator": "Heiko Jansen (hbz), Ingolf Kuss (hbz), Bernhard Assmann (hbz)",
+	"target": "https?://.*\\.digibib\\.net/(Digibib|jumpto|opensearch)",
+	"minVersion": "2.1.9",
+	"maxVersion": null,
+	"priority": 100,
+	"inRepository": true,
+	"browserSupport": "g",
+	"lastUpdated": "2012-03-13 01:20:00"
 }
 
 /*
@@ -31,7 +32,6 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 /*
  * Check if the current page really is a page with results (hits) in it.
  * Also checks for the type of results because the different lists have
@@ -41,26 +41,20 @@
 function detectWeb(doc, url) {
 	var namespace = doc.documentElement.namespaceURI;
 
-	var indicator = doc.evaluate('/html/body//span[@id="zotero"]',
-			doc,
-			null,
-			XPathResult.FIRST_ORDERED_NODE_TYPE,
-			null )
-		.singleNodeValue;
+	var indicator = doc.evaluate('/html/body//span[@id="zotero"]', doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
 	var indicator_class = indicator.getAttribute('class');
-	if ( indicator_class.match(/multi/) ) {
+	if (indicator_class.match(/multi/)) {
 		// There's one or more result list each containing one or more hits in this page
 		return "multiple";
 	}
-	if ( indicator_class.match(/single/) ) {
+	if (indicator_class.match(/single/)) {
 		// There's one hit in this page
 		return "single";
 	}
 
 	return "";
 }
-
 
 /*
  * Present a list of importable hits in the current page to the user.
@@ -69,14 +63,9 @@ function detectWeb(doc, url) {
  *
  **/
 function doWeb(doc, url) {
-	var namespace  = doc.documentElement.namespaceURI;
+	var namespace = doc.documentElement.namespaceURI;
 
-	var indicator = doc.evaluate('/html/body//span[@id="zotero"]',
-			doc,
-			null,
-			XPathResult.ANY_TYPE,
-			null )
-		.iterateNext();
+	var indicator = doc.evaluate('/html/body//span[@id="zotero"]', doc, null, XPathResult.ANY_TYPE, null).iterateNext();
 	var indicator_class = indicator.getAttribute('class');
 
 	var availableItems = new Object();
@@ -84,42 +73,48 @@ function doWeb(doc, url) {
 	var conf = new Object;
 	var type = '';
 	// configure the extraction method
-	if ( indicator_class.match(/multi/) ) {
+	if (indicator_class.match(/multi/)) {
 		// There's one or more result list each containing one or more hits in this page
-		if ( indicator_class.match(/z_metasearch_multi/) ) {
-			conf["lists_xpath"]	  = '//div[contains(@class,"listcontainer")]';
-			conf["item_xpath"]	   = './dl[contains(@class,"list")]/dt';
+		if (indicator_class.match(/z_metasearch_multi/)) {
+			conf["lists_xpath"] = '//div[contains(@class,"listcontainer")]';
+			conf["item_xpath"] = './dl[contains(@class,"list")]/dt';
 			conf["item_title_xpath"] = './a';
-			conf["item_url_xpath"]   = 'following-sibling::dd[contains(@class,"func")]//a[contains(@class,"js_formatExport")]';
-			conf["url_trnsfrm"]	  = function (hitUrl) { return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/,'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/,'FORMAT=MODS'); };
-		}
-		else if ( indicator_class.match(/z_ezb_multi/) ) {
-			conf["lists_xpath"]	  = '//div[contains(@class,"listcontainer")]';
-			conf["item_xpath"]	   = './dl[contains(@class,"list")]/dt';
+			conf["item_url_xpath"] = 'following-sibling::dd[contains(@class,"func")]//a[contains(@class,"js_formatExport")]';
+			conf["url_trnsfrm"] = function (hitUrl) {
+				return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/, 'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/, 'FORMAT=MODS');
+			};
+		} else if (indicator_class.match(/z_ezb_multi/)) {
+			conf["lists_xpath"] = '//div[contains(@class,"listcontainer")]';
+			conf["item_xpath"] = './dl[contains(@class,"list")]/dt';
 			conf["item_title_xpath"] = './a[contains(@class,"js_getFull")]';
-			conf["item_url_xpath"]   = './ul/li/a[contains(@class,"js_formatExport")]';
-			conf["url_trnsfrm"]	  = function (hitUrl) { return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/,'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/,'FORMAT=MODS'); };
-		}
-		else if ( indicator_class.match(/z_digilink_multi/) ) {
-			conf["lists_xpath"]	  = '//div[contains(@class,"listcontainer")]';
-			conf["item_xpath"]	   = './dl[contains(@class,"list")]/dt';
+			conf["item_url_xpath"] = './ul/li/a[contains(@class,"js_formatExport")]';
+			conf["url_trnsfrm"] = function (hitUrl) {
+				return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/, 'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/, 'FORMAT=MODS');
+			};
+		} else if (indicator_class.match(/z_digilink_multi/)) {
+			conf["lists_xpath"] = '//div[contains(@class,"listcontainer")]';
+			conf["item_xpath"] = './dl[contains(@class,"list")]/dt';
 			conf["item_title_xpath"] = './strong';
-			conf["item_url_xpath"]   = 'following-sibling::dd[2]/ul/li/a[contains(@class,"js_formatExport")]';
-			conf["url_trnsfrm"]	  = function (hitUrl) { return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/,'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/,'FORMAT=MODS'); };
-		}
-		else if ( indicator_class.match(/z_dbis_multi/) ) {
-			conf["lists_xpath"]	  = '//div[contains(@class,"listcontainer")]';
-			conf["item_xpath"]	   = './dl[contains(@class,"list")]/dt';
+			conf["item_url_xpath"] = 'following-sibling::dd[2]/ul/li/a[contains(@class,"js_formatExport")]';
+			conf["url_trnsfrm"] = function (hitUrl) {
+				return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/, 'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/, 'FORMAT=MODS');
+			};
+		} else if (indicator_class.match(/z_dbis_multi/)) {
+			conf["lists_xpath"] = '//div[contains(@class,"listcontainer")]';
+			conf["item_xpath"] = './dl[contains(@class,"list")]/dt';
 			conf["item_title_xpath"] = './a[contains(@class,"js_getFull")]';
-			conf["item_url_xpath"]   = './/a[contains(@class,"js_formatExport")]';
-			conf["url_trnsfrm"]	  = function (hitUrl) { return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/,'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/,'FORMAT=MODS'); };
-		}
-		else if ( indicator_class.match(/z_cart_multi/) ) {
-			conf["lists_xpath"]	  = '//div[contains(@class,"listcontainer")]';
-			conf["item_xpath"]	   = './dl[contains(@class,"list")]/dt';
+			conf["item_url_xpath"] = './/a[contains(@class,"js_formatExport")]';
+			conf["url_trnsfrm"] = function (hitUrl) {
+				return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/, 'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/, 'FORMAT=MODS');
+			};
+		} else if (indicator_class.match(/z_cart_multi/)) {
+			conf["lists_xpath"] = '//div[contains(@class,"listcontainer")]';
+			conf["item_xpath"] = './dl[contains(@class,"list")]/dt';
 			conf["item_title_xpath"] = 'child::*[2]';
-			conf["item_url_xpath"]   = 'following-sibling::dd[3]//a[contains(@class,"js_formatExport")]';
-			conf["url_trnsfrm"]	  = function (hitUrl) { return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/,'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/,'FORMAT=MODS'); };
+			conf["item_url_xpath"] = 'following-sibling::dd[3]//a[contains(@class,"js_formatExport")]';
+			conf["url_trnsfrm"] = function (hitUrl) {
+				return hitUrl.replace(/SUBSERVICE=DIRECT_EXPORT/, 'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/, 'FORMAT=MODS');
+			};
 		}
 		availableItems = _get_items_multi(doc, url, namespace, conf);
 
@@ -134,82 +129,54 @@ function doWeb(doc, url) {
 			Zotero.Utilities.doGet(fetchThese, importItem);
 			Zotero.wait();
 		});
-	}
-	else if ( indicator_class.match(/z_[a-z]*_single/) ) {
-		var urlNode = doc.evaluate(
-				'/html/body/div/div/div[@id="main"]//a[contains(@class,"js_formatExport")]',
-				doc,
-				null,
-				XPathResult.FIRST_ORDERED_NODE_TYPE,
-				null
-				);
-		if ( urlNode ) {
-			var hitUrl = urlNode.singleNodeValue.href.replace(/SUBSERVICE=DIRECT_EXPORT/,'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/,'FORMAT=MODS');
+	} else if (indicator_class.match(/z_[a-z]*_single/)) {
+		var urlNode = doc.evaluate('/html/body/div/div/div[@id="main"]//a[contains(@class,"js_formatExport")]', doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+		if (urlNode) {
+			var hitUrl = urlNode.singleNodeValue.href.replace(/SUBSERVICE=DIRECT_EXPORT/, 'SUBSERVICE=FORMAT_EXPORT').replace(/FORMAT=TXT/, 'FORMAT=MODS');
 			Zotero.Utilities.doGet(hitUrl, importItem);
 			Zotero.wait();
 		}
 	}
 }
 
-
 /*
  * Generic extraction of a list of records (title/url pairs) from the page
  * guided by a set of XPaths addressing the relevant html nodes.
  *
  **/
-function _get_items_multi (doc, url, namespace, conf) {
+function _get_items_multi(doc, url, namespace, conf) {
 	var myItems = new Object;
 
 	// one or more lists in this page
 	var listsSnapshot = doc.evaluate(
-			conf["lists_xpath"],
-			doc,
-			null,
-			XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-			null
-			);
+	conf["lists_xpath"], doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 	var isSingleEntryList = listsSnapshot.snapshotLength == 1 ? true : false;
 
 	// walk over every list
-	for ( var i = 0; i < listsSnapshot.snapshotLength; i++ ) {
+	for (var i = 0; i < listsSnapshot.snapshotLength; i++) {
 		var cur_list = listsSnapshot.snapshotItem(i);
 
 		// get the entries in the current list
 		var listEntriesSnapshot = doc.evaluate(
-				conf["item_xpath"],
-				cur_list,
-				null,
-				XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-				null
-				);
+		conf["item_xpath"], cur_list, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
 		// for every entry extract title and url and store these pairs
 		// in an hash object; keys are the urls
-		for ( var j = 0; j < listEntriesSnapshot.snapshotLength; j++ ) {
+		for (var j = 0; j < listEntriesSnapshot.snapshotLength; j++) {
 			var cur_entry = listEntriesSnapshot.snapshotItem(j);
 			var titleNode = doc.evaluate(
-					conf["item_title_xpath"],
-					cur_entry,
-					null,
-					XPathResult.FIRST_ORDERED_NODE_TYPE,
-					null
-					);
+			conf["item_title_xpath"], cur_entry, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
 
 			var title, hitUrl;
 
-			if ( titleNode.singleNodeValue == null ) {
+			if (titleNode.singleNodeValue == null) {
 				continue;
 			}
 			title = _formatTitle(isSingleEntryList, i, j, titleNode.singleNodeValue.textContent);
 
 			var urlNode = doc.evaluate(
-					conf["item_url_xpath"],
-					cur_entry,
-					null,
-					XPathResult.FIRST_ORDERED_NODE_TYPE,
-					null
-					);
-			if ( titleNode.singleNodeValue == null ) {
+			conf["item_url_xpath"], cur_entry, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+			if (titleNode.singleNodeValue == null) {
 				continue;
 			}
 
@@ -226,16 +193,14 @@ function _get_items_multi (doc, url, namespace, conf) {
  * with a number
  *
  **/
-function _formatTitle (isSingleEntryList, num_a, num_b, text) {
+function _formatTitle(isSingleEntryList, num_a, num_b, text) {
 	var prefix = '';
 	num_a++;
 	num_b++;
-	if (isSingleEntryList)
-		prefix = ( num_b < 10 ? "0" + num_b : num_b ) + ": "; 
-	else
-		prefix = ( num_a < 10 ? "0" + num_a : num_a ) + "." + ( num_b < 10 ? "0" + num_b : num_b ) + ": ";
+	if (isSingleEntryList) prefix = (num_b < 10 ? "0" + num_b : num_b) + ": ";
+	else prefix = (num_a < 10 ? "0" + num_a : num_a) + "." + (num_b < 10 ? "0" + num_b : num_b) + ": ";
 
-	text = ( text.length > 80 ) ? text.substr( 0, 77 ) + "..." : text;
+	text = (text.length > 80) ? text.substr(0, 77) + "..." : text;
 
 	return prefix + text;
 }
@@ -244,7 +209,7 @@ function _formatTitle (isSingleEntryList, num_a, num_b, text) {
  * Import a record to Zotero using the standard MODS translator.
  *
  **/
-function importItem (mods, resp, url) {
+function importItem(mods, resp, url) {
 	// import to zotero by relying on standard MODS translator
 	var translator = Zotero.loadTranslator("import");
 	translator.setTranslator("0e2235e7-babf-413c-9acf-f27cce5f059c"); // MODS-translator
@@ -258,18 +223,10 @@ function importItem (mods, resp, url) {
  * has converted the records
  *
  **/
-function cleanup (obj, item) {
-	if ( item.archiveLocation ) {
+function cleanup(obj, item) {
+	if (item.archiveLocation) {
 		item.callNumber = item.archiveLocation;
 		item.archiveLocation = "";
 	}
-
-	for ( i in item.creators ) {
-		if ( item.creators[i].firstName ) {
-			item.creators[i].fieldMode = 2;
-		}
-	}
-
 	item.complete();
 }
-
